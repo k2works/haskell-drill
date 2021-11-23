b2ebca3c439e80f92c04ff543143efc7c595e2f2
k2works <kakimomokkuri@gmail.com>
2021-11-23 14:06:25 +0900

    test: 再帰

--- src/SevenDays.hs
+++ src/SevenDays.hs
@@ -2,3 +2,12 @@ module SevenDays where
 
 double :: Integer -> Integer
 double x = x + x
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial x = x * factorial (x - 1)
+
+fact_with_guard :: Integer -> Integer
+fact_with_guard x
+  | x > 1 = x * fact_with_guard (x - 1)
+  | otherwise = 1
--- test/SevenDaysSpec.hs
+++ test/SevenDaysSpec.hs
@@ -8,3 +8,11 @@ spec = do
   describe "double" $ do
     it "2倍にして返す" $
       double 5 `shouldBe` 10
+
+  describe "factorial" $ do
+    it "3ならば6を返す" $
+      factorial 3 `shouldBe` 6
+
+  describe "fact_with_guard" $ do
+    it "3ならば6を返す" $
+      fact_with_guard 3 `shouldBe` 6
