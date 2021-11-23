8d1eebc39746736d9b290e87a5259de2f18af52c
k2works <kakimomokkuri@gmail.com>
2021-11-23 14:23:00 +0900

    test: タプルとリスト

--- src/SevenDays.hs
+++ src/SevenDays.hs
@@ -11,3 +11,13 @@ fact_with_guard :: Integer -> Integer
 fact_with_guard x
   | x > 1 = x * fact_with_guard (x - 1)
   | otherwise = 1
+
+fibTuple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+fibTuple (x, y, 0) = (x, y, 0)
+fibTuple (x, y, index) = fibTuple (y, x + y, index - 1)
+
+fibResult :: (Integer, Integer, Integer) -> Integer
+fibResult (x, y, z) = x
+
+fib :: Integer -> Integer
+fib x = fibResult (fibTuple (0, 1, x))
--- test/SevenDaysSpec.hs
+++ test/SevenDaysSpec.hs
@@ -16,3 +16,10 @@ spec = do
   describe "fact_with_guard" $ do
     it "3ならば6を返す" $
       fact_with_guard 3 `shouldBe` 6
+  describe "タプルとリスト" $ do
+    it "タプルを返す" $
+      fibTuple (0, 1, 4) `shouldBe` (3, 5, 0)
+    it "タプルの一番目を取り出す" $
+      fibResult (0, 1, 4) `shouldBe` 0
+    it "フィボナッチ数を返す" $
+      fib 100 `shouldBe` 354224848179261915075
